830b24f13d4ddf4b28152f5fa36901b0d0ca95cb
wyong <wyong@bab8edee-72ee-46bd-b9b3-b7cbb2ae0fd4>
2017-08-05 00:08:17 +0000

    多数据源事务处理有问题。。暂时改成直接用DBHelper.class 类中SQL操作。

--- project/game28/notify/src/com/game/service/impl/OrderServiceImpl2.java
+++ project/game28/notify/src/com/game/service/impl/OrderServiceImpl2.java
@@ -1,26 +1,105 @@
 package com.game.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import com.game.dao.OrderDao2;
 import com.game.entity.EcsOrderInfo;
 import com.game.service.OrderService2;
+import com.game.utils.DBhelper;
 
 @Service("orderService2")
 public class OrderServiceImpl2 implements OrderService2{
 
-	@Autowired
-	OrderDao2 orderDao;
-	
+	static final String ORDER_FIND_SQL = " SELECT order_id,order_sn,goods_amount FROM  ecs_order_info WHERE order_sn ='%s' ";
+	static final String ORDER_UPDATE_SQL = " UPDATE  ecs_order_info SET order_status=1 ,pay_status=2 ,shipping_status =3  WHERE order_sn ='%s' ";
 	@Override
 	public EcsOrderInfo findByOrdersn(String ordersn) {
-		return orderDao.findOrderBysn(ordersn);
+		if(StringUtils.isBlank(ordersn)) {
+			return null;
+		}
+		EcsOrderInfo info = null;
+		Connection conn = null;
+		PreparedStatement ps = null; 
+		ResultSet rs = null;
+		try {
+			conn = DBhelper.getConn2();
+			ps = conn.prepareStatement(String.format(ORDER_FIND_SQL, ordersn));
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				info = new EcsOrderInfo() ;
+				info.setOrder_sn(ordersn);
+				info.setOrder_id(rs.getInt("order_id"));
+				info.setOrder_amount(rs.getBigDecimal("goods_amount"));
+			}
+			return info;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(!rs.isClosed()) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				if(!ps.isClosed()) {
+					ps.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				if(conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+		
 	}
 
 	@Override
 	public void updateEcsOrderStatus(EcsOrderInfo orderinfo) {
-		orderDao.updateEntity(orderinfo);
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try {
+			if(orderinfo!=null && StringUtils.isNotBlank(orderinfo.getOrder_sn())) {
+				conn = DBhelper.getConn2();
+				ps = conn.prepareStatement(String.format(ORDER_UPDATE_SQL, orderinfo.getOrder_sn()));
+				ps.executeUpdate();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(!ps.isClosed()) {
+					ps.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				if(conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
