6688ce1461cbcdeb87a8fc8ad3be210683865675
wyong <wyong@bab8edee-72ee-46bd-b9b3-b7cbb2ae0fd4>
2017-08-07 04:02:18 +0000

    项目初始化，调用initBeanFactory来初始化需要异步通知的数据。

---
+++ src/com/game/serverutil/excutor/InitBeanFactory.java
@@ -0,0 +1,35 @@
+package com.game.serverutil.excutor;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+/**
+ * @author wrong1111
+ *项目起动时，初始化一些数据。
+ */
+public class InitBeanFactory implements ApplicationListener<ContextRefreshedEvent>{
+
+	Logger logger  = Logger.getLogger(getClass());
+	private static volatile boolean initialled = false;  
+
+	@Autowired
+	NotifyThread notifyThread;
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		System.out.println("=============");
+		synchronized(InitBeanFactory.class) {  
+	        if(event.getApplicationContext().getParent() != null && initialled) {  
+	            return;  
+	        }  
+	        initialled = true;  
+	    }  
+		notifyThread.startThread();
+	}
+	 
+	  
+	 
+
+}
--- src/com/game/serverutil/excutor/NotifyThread.java
+++ src/com/game/serverutil/excutor/NotifyThread.java
@@ -3,69 +3,82 @@ package com.game.serverutil.excutor;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.DelayQueue;
-import java.util.concurrent.ThreadPoolExecutor;
+
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 
 import com.game.pojo.NotifyVo;
 import com.game.service.PayService;
 import com.game.utils.common.dao.support.Page;
 
-
+@Component("notifyThread")
 public class NotifyThread {
+	Logger logger = Logger.getLogger(NotifyThread.class);
 	
 	public static DelayQueue<NotifyTask> tasks = new DelayQueue<NotifyTask>();
-	 
-	 
-	private ThreadPoolTaskExecutor taskExecutor ; 
+ 
+	@Autowired
+	PayService payService;
 	
-	 Logger logger = Logger.getLogger(NotifyThread.class);
+	@Autowired
+	NotifyQueue notifyQueue;
 	
+	 /**
+	  * 守护线程
+	  */
+	private Thread daemonThread;
+	public void init() {
+        daemonThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					try {
+						System.out.println("DeamonThread is sleeping 5s...");
+						Thread.sleep(10000);
+						runTask();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+        	
+        });
+        daemonThread.setDaemon(true);
+        daemonThread.setName("DelayQueueTask");
+        daemonThread.start();
+	}
+	
+	private void runTask() {
+		System.out.println("runTask-----");
+		ThreadPoolTaskExecutor taskE = (ThreadPoolTaskExecutor) SpringContext.getBean("taskExecutor");
+		 // 如果当前活动线程等于最大线程，那么不执行
+        if (taskE.getActiveCount() < taskE.getMaxPoolSize()) {
+        	System.out.println("noitfy-thread-task>>>>>>>>> 队列长度:"+ tasks.size()+"--");
+            final NotifyTask task = tasks.poll();
+            if (task != null) {
+            	taskE.execute(new Runnable() {
+                    public void run() {
+                        logger.error("noitfy-thread-"+taskE.getActiveCount() + "---------");
+                        tasks.remove(task);
+                        task.run();
+                    }
+                });
+            }
+        }
+	}
 	public  void startThread() {
         logger.error("notify-thread-startThread");
-        taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(3);
-        taskExecutor.setMaxPoolSize(10);
-        taskExecutor.setQueueCapacity(100);
-        taskExecutor.setKeepAliveSeconds(300);
-        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        
-        taskExecutor.execute(new Runnable() {
-            public void run() {
-                try {
-                    while (true) {
-                        Thread.sleep(50);//50毫秒执行一次
-                        // 如果当前活动线程等于最大线程，那么不执行
-                        if (taskExecutor.getActiveCount() < taskExecutor.getMaxPoolSize()) {
-                        	 logger.error("noitfy-thread-task 队列长度:"+ tasks.size()+"--");
-                            final NotifyTask task = tasks.poll();
-                            if (task != null) {
-                            	taskExecutor.execute(new Runnable() {
-                                    public void run() {
-                                        logger.error("noitfy-thread-"+taskExecutor.getActiveCount() + "---------");
-                                        tasks.remove(task);
-                                        task.run();
-                                    }
-                                });
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                	logger.error("noitfy-thread-系统异常",e);
-                    e.printStackTrace();
-                }
-            }
-        });
+        init();
         startFromDb();
     }
 
 	private void startFromDb() {
-		
 		   int pageNum = 1;
 		   int numPerPage = 200;
-		   PayService payService  = (PayService)SpringContext.getBean("payService");
-		   NotifyQueue notifyQueue = (NotifyQueue)SpringContext.getBean("notifyQueue");
 		   Page page =  payService.findNotify2Send("notify", pageNum, numPerPage);
 		    int totalSize = (page.getTotalPageCount()-1)/page.getPageSize()+1;//总页数
 	        while (pageNum <= totalSize) {
